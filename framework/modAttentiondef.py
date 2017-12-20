f9fdda8be8fb538e911ebb15b372da80da16e528
JNaj <judy.najnudel@gmail.com>
2017-12-20 19:58:35 +0100

    rnn operationnel, bonne reconstruction

--- framework/modAttentiondef.py
+++ framework/modAttentiondef.py
@@ -20,6 +20,7 @@ class AttentionRnn(nn.Module):
             sample_size: size of the samples
             h_dim: dimension of the hidden state and the attention matrices
             z_dim: dimension of the encoded space
+            batch_size: size of the minibatch
         Returns:
             Attention object
 
@@ -151,4 +152,5 @@ class AttentionRnn(nn.Module):
         Returns:
             - output: decoded vector
         """
-        return self.decoder(latent_vec)
+        sampled, _ = self.decoder(latent_vec)
+        return sampled
--- main_rnn.py
+++ main_rnn.py
@@ -30,13 +30,13 @@ BATCH_SIZE = 50
 #LEN_EXAMPLES = 38400
 LEN_EXAMPLES = 2000
 # Net parameters
-Z_DIM, H_DIM = 20, 400
+Z_DIM, H_DIM = 30, 400
 FS = 8000
 
 # %% Importing DATASET
 
 # Creating dataset
-DATASET_FILEPATH = 'data/datasets/DATASET_test.obj'
+DATASET_FILEPATH = 'data/datasets/DATASET_full_small.obj'
 
 # If there is no archive of the dataset, it needs to be rendered
 if not pa.isfile(DATASET_FILEPATH):
@@ -44,7 +44,7 @@ if not pa.isfile(DATASET_FILEPATH):
     DATASET = dts.toyDataset(length_sample=LEN_EXAMPLES,
                              n_bins=N_FFT,
                              Fe_Hz=FS,
-                             data='cqt')
+                             data='spectro')
     obj = DATASET
     file_obj = open(DATASET_FILEPATH, 'w')
     pickle.dump(obj, file_obj)
@@ -78,12 +78,13 @@ NB_FEN = WIDTH/N_FFT
 FIXED_X = to_var(FIXED_X)
 reconst_images = FIXED_X.view(BATCH_SIZE, 1, N_FFT, -1)
 torchvision.utils.save_image(reconst_images.data.cpu(),
-                             './data/CQT/original_images.png')
+                             './data/spectro/original_images.png')
 
 # formatting for rnn
-FIXED_X = torch.chunk(FIXED_X, NB_FEN, 1)
+FIXED_X = torch.chunk(FIXED_X, N_FFT, 1)
 FIXED_X = torch.cat(FIXED_X, 0)
-FIXED_X = FIXED_X.view(NB_FEN, BATCH_SIZE, -1)
+FIXED_X = FIXED_X.view(N_FFT, BATCH_SIZE, -1)
+FIXED_X = FIXED_X.transpose(0, 2)
 # %% CREATING THE Beta-VAE
 betaVAE = modAttentiondef.AttentionRnn(sample_size=N_FFT, h_dim=H_DIM,
                                        z_dim=Z_DIM)
@@ -102,7 +103,8 @@ if torch.cuda.is_available():
 OPTIMIZER = torch.optim.Adam(betaVAE.parameters(), lr=0.001)
 
 ITER_PER_EPOCH = len(DATASET)/BATCH_SIZE
-NB_EPOCH = 100
+NB_EPOCH = 1
+SOUND_LENGTH = np.shape(DATASET.__getitem__(9)[0])[0]
 
 # %%
 """ TRAINING """
@@ -118,20 +120,24 @@ for epoch in range(NB_EPOCH):
         # Formatting
         images = to_var(torch.Tensor(images.float()).squeeze())
         batch_size = images.size(0)
-        images = torch.chunk(images, NB_FEN, 1)
+        images = torch.chunk(images, N_FFT, 1)
         images = torch.cat(images, 0)
-        images = images.view(NB_FEN, batch_size, -1)
+        images = images.view(N_FFT, batch_size, -1)
+        images = images.transpose(0, 2)
         out, [yt, st], mu, log_var = betaVAE(images)
 
         # Compute reconstruction loss and KL-divergence
-        reconst_loss = F.binary_cross_entropy(out, images, size_average=True)
+        reconst_loss = -0.5*N_FFT*batch_size*torch.sum(2*np.pi*log_var)
+        reconst_loss -= torch.sum(torch.sum((images-out).pow(2))/((2*log_var.exp())))
+#        #reconst_loss /= (BATCH_SIZE*SOUND_LENGTH)
+#        reconst_loss = F.binary_cross_entropy(out, images, size_average=True)
 
         kl_divergence = torch.sum(0.5 * (mu**2
                                          + torch.exp(log_var)
                                          - log_var - 1))
 
         # Backprop + Optimize
-        total_loss = reconst_loss + BETA*kl_divergence
+        total_loss = -reconst_loss + BETA*kl_divergence
         OPTIMIZER.zero_grad()
         total_loss.backward()
         OPTIMIZER.step()
@@ -148,26 +154,32 @@ for epoch in range(NB_EPOCH):
 
     # Save the reconstructed images
     reconst_images, _, _, _ = betaVAE(FIXED_X)
-    reconst_images = torch.chunk(reconst_images, NB_FEN, 0)
+    reconst_images = reconst_images.transpose(0, 2)
+    reconst_images = torch.chunk(reconst_images, N_FFT, 0)
     reconst_images = torch.cat(reconst_images, 2).squeeze()
     reconst_images = reconst_images.view(BATCH_SIZE, 1, N_FFT, -1)
     torchvision.utils.save_image(reconst_images.data.cpu(),
-                                 './data/CQT/reconst_images_%d.png' %(epoch+1))
+                                 './data/spectro/reconst_images_%d.png' %(epoch+1))
 
-#%% SAMPLING FROM LATENT SPACE
-#FIXED_Z = zdim_analysis(BATCH_SIZE, Z_DIM, 50, -20, 20)
-#FIXED_Z = to_var(torch.Tensor(FIXED_Z.contiguous()))
-#
-## Sampling from model, reconstructing from spectrogram
-#sampled_image = betaVAE.sample(FIXED_Z)
-#reconst_images = sampled_image.view(BATCH_SIZE, 1, N_FFT, -1)
-#torchvision.utils.save_image(reconst_images.data.cpu(), './data/CQT/sampled_images.png')
-#
-##%%
+# %% SAMPLING FROM LATENT SPACE
+FIXED_Z = zdim_analysis(BATCH_SIZE, Z_DIM, 2, 0, 10)
+FIXED_Z = to_var(torch.Tensor(FIXED_Z.contiguous()))
+FIXED_Z = FIXED_Z.repeat(NB_FEN, 1)
+FIXED_Z = FIXED_Z.view(NB_FEN, BATCH_SIZE, Z_DIM)
+
+# Sampling from model, reconstructing from spectrogram
+sampled_image = betaVAE.sample(FIXED_Z)
+sampled_image = sampled_image.transpose(0, 2)
+sampled_image = torch.chunk(sampled_image, N_FFT, 0)
+sampled_image = torch.cat(sampled_image, 2).squeeze()
+sampled_image = sampled_image.view(BATCH_SIZE, 1, N_FFT, -1)
+torchvision.utils.save_image(sampled_image.data.cpu(),
+                             './data/spectro/sampled_images.png')
+
+# %%
 #sampled_image_numpy = sampled_image.data.numpy()
-#sampled_image_numpy =sampled_image_numpy[1,:].reshape(N_FFT/2+1,-1)
+#sampled_image_numpy = sampled_image_numpy[1, :].reshape(N_FFT/2+1, -1)
 #
 #reconst_sound = DATASET.audio_engine.griffinlim(sampled_image_numpy, N_iter=500)
 #output_name = 'sampled_sound.wav'
-#librosa.output.write_wav('data/SOUND/sampled_sound.wav',reconst_sound,DATASET.Fs)
-
+#librosa.output.write_wav('data/SOUND/sampled_sound.wav', reconst_sound,DATASET.Fs)
